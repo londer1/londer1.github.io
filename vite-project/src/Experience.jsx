fdbe69a626d868c994fccbcf9b3e3c5d440bc9e8
londer <148044380+londer1@users.noreply.github.com>
2025-10-28 15:42:45 +0100

    Updates

--- vite-project/src/Experience.jsx
+++
@@ -1,9 +0,0 @@
-import { ScrollReveal } from '@appletosolutions/reactbits';
-
-export default function Experience() {
-  return (
-    <ScrollReveal>
-      <div>Arbeidserfaring: Lærling hos Utdanningsdirektoratet</div>
-    </ScrollReveal>
-  );
-}
--- vite-project/src/Skills.jsx
+++
@@ -1,5 +0,0 @@
-import { FallingText } from '@appletosolutions/reactbits';
-
-export default function Skills() {
-  return <FallingText text="Motivert, Nysgjerrig, Lærevillig" />;
-}
